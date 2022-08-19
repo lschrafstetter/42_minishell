9b99a0a2d19fc615ab04b7bedb9b4e659d90b02b
lschrafs <lschrafs@student.42wolfsburg.de>
2022-08-19 14:46:37 +0200

    L: now really fixed red

--- src/parsing/ms_pa.c
+++ src/parsing/ms_pa.c
@@ -6,36 +6,36 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 12:01:04 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/08/19 14:16:17 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/08/19 14:45:49 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static int	is_red_without_whitespace(t_lst_str **ls, t_process *proc)
+static int	is_red_without_whitespace(char *str, t_process *proc)
 {
-	if (ft_strncmp(temp->str, "<<", 2) == 0 && temp->str + 2 != '<')
+	if (ft_strncmp(str, "<<", 2) == 0 && *(str + 2) != '<')
 	{
 		ls_red_addback(proc->ls_red, \
-							ls_red_new("<<", temp->next->str + 2));
+							ls_red_new("<<", str + 2));
 		return (1);
 	}
-	else if (ft_strncmp(temp->str, ">>", 2) == 0 && temp->str + 2 != '>>')
+	else if (ft_strncmp(str, ">>", 2) == 0 && *(str + 2) != '>')
 	{
 		ls_red_addback(proc->ls_red, \
-							ls_red_new(">>", temp->next->str + 2));
+							ls_red_new(">>", str + 2));
 		return (1);
 	}
-	else if (ft_strncmp(temp->str, "<", 1) == 0 && temp->str + 1 != '<')
+	else if (ft_strncmp(str, "<", 1) == 0 && *(str + 1) != '<')
 	{
 		ls_red_addback(proc->ls_red, \
-							ls_red_new("<", temp->next->str + 1));
+							ls_red_new("<", str + 1));
 		return (1);
 	}
-	else if (ft_strncmp(temp->str, ">", 1) == 0 && temp->str + 1 != '>')
+	else if (ft_strncmp(str, ">", 1) == 0 && *(str + 1) != '>')
 	{
 		ls_red_addback(proc->ls_red, \
-							ls_red_new(">", temp->next->str + 1));
+							ls_red_new(">", str + 1));
 		return (1);
 	}
 	return (0);
@@ -59,7 +59,7 @@ static void	sort_cmd_red(t_lst_str **ls, t_process *proc)
 			if (!temp->next)
 				break ;
 		}
-		else if (is_red_without_whitespace(ls, proc))
+		else if (is_red_without_whitespace(temp->str, proc))
 		{
 			temp = temp->next;
 			continue ;
